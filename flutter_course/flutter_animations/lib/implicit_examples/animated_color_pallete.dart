180d64f3d5cc18693d7ab9cebc468a0a7f3fb105
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-10-14 21:06:11 +0100

    restarted with animatiions

--- flutter_course/flutter_animations/lib/implicit_examples/animated_color_pallete.dart
+++ flutter_course/flutter_animations/lib/implicit_examples/animated_color_pallete.dart
@@ -1,59 +1,62 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:flutter/material.dart';
+
 class AnimatedColorPalette extends StatefulWidget {
-  const AnimatedColorPalette({super.key});
+
 
   @override
   State<AnimatedColorPalette> createState() => _AnimatedColorPaletteState();
 }
 
+
 class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
+
   List<Color> currentPalette = generateRandomPalette();
 
   static List<Color> generateRandomPalette() {
     final random = Random();
     return List.generate(
-      5,
-          (_) => Color.fromRGBO(
-        random.nextInt(256),  // for R
-        random.nextInt(256),  // for G
-        random.nextInt(256),  // for B
-        1,                    // for O opacity
-      ),
-    );
+        4,
+            (_) => Color.fromRGBO(
+            random.nextInt(256),
+            random.nextInt(256),
+            random.nextInt(256),
+            1
+        ));
   }
+
 
   void regeneratePalette() {
     setState(() {
       currentPalette = generateRandomPalette();
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Color Palette Generator'),
+        title: Text('Animated Color Pallete'),
       ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             for (Color color in currentPalette)
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 800),
-                curve: Curves.linear,
-                width: 100,
-                height: 100,
-                color: color,
-                margin: const EdgeInsets.all(8),
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 1000),
 
-              ),
+                  width: 100,
+                  height: 90,
+                  color: color,
+                ),
+
+            SizedBox(height: 20,),
             ElevatedButton(
-              onPressed: regeneratePalette,
-              child: const Text('Generate New Palette'),
-            ),
+                onPressed: regeneratePalette,
+                child: const Text('Generate new Pallet'))
           ],
         ),
       ),
