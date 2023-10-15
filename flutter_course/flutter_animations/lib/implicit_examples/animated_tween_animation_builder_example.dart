339347f76a8c5f1d0f875a84cc82cdf1eafc729d
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-10-15 13:50:17 +0100

    done with implicit animations

--- flutter_course/flutter_animations/lib/implicit_examples/animated_tween_animation_builder_example.dart
+++ flutter_course/flutter_animations/lib/implicit_examples/animated_tween_animation_builder_example.dart
@@ -24,21 +24,29 @@ class _PulsatingCircleAnimationState extends State<PulsatingCircleAnimation> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 500),
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.blue,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.blue.withOpacity(0.5),
-                blurRadius: size,
-                spreadRadius: size / 2,
-              )
-            ]
-          ),
+        child: TweenAnimationBuilder(
+          duration: Duration(milliseconds: 3500),
+          tween: Tween<double>(begin: 0, end: 200),
+          builder: (context, object, widget){
+            return Container(
+              width: size,
+              height: size,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue.withOpacity(0.5),
+                      blurRadius: size,
+                      spreadRadius: size / 2,
+                    )
+                  ]
+              ),
+                child: widget
+            );
+          },
+
+          child: const Text('Today is a good day!'),
         ),
       ),
     );
