50a0f6a0a2fe238a90cd8fed9e47a83deaa9752d
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-10-15 14:44:44 +0100

    done with login_animations

--- flutter_course/flutter_animations/lib/explicit_animations/login_animations.dart
+++ flutter_course/flutter_animations/lib/explicit_animations/login_animations.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class LoginScreenAnimation extends StatefulWidget {
   const LoginScreenAnimation({super.key});
 
@@ -9,24 +10,29 @@ class LoginScreenAnimation extends StatefulWidget {
 
 class _LoginScreenAnimationState extends State<LoginScreenAnimation> with SingleTickerProviderStateMixin {
 
-  late AnimationController controller;
-  late Animation<double> opacityAnimation;
-
+  late Animation<double> opacity;
+  late AnimationController animationController;
   late Animation<Offset> slideAnimation;
+
   late Animation<double> scaleAnimation;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 1500));
-    opacityAnimation = Tween<double>(begin: 0, end: 1).animate(controller);
+    animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 2000));
+    opacity = Tween<double>(begin: 0, end: 1).animate(animationController);
 
+    animationController.addListener(() {
+      if(animationController.isCompleted){
+        animationController.reverse();
+      }
+    });
 
-    slideAnimation = Tween(begin: Offset(-1, -1), end: Offset(0, 0)).animate(CurvedAnimation(parent: controller, curve: Curves.ease));
+    slideAnimation = Tween(begin: Offset(-1, -1), end: Offset.zero).animate(CurvedAnimation(parent: animationController, curve: Curves.ease));
+    animationController.forward();
 
-    scaleAnimation = Tween<double>(begin: 0, end: 1).animate(controller);
-    controller.forward();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,50 +41,35 @@ class _LoginScreenAnimationState extends State<LoginScreenAnimation> with Single
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FadeTransition(
-              opacity: opacityAnimation,
-              child: const FlutterLogo(
-                size: 100.0,
-              ),
-            ),
-            Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: SlideTransition(
-                  position: slideAnimation,
-                  child: ScaleTransition(
-                    scale: scaleAnimation,
-                    child: Column(
-                      children: [
-                        const TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Username',
-                          ),
-                        ),
+                opacity: opacity,
+                child: const FlutterLogo(size: 100,)),
 
-                        const SizedBox(height: 16.0,),
-                        const TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                          ),
-                        ),
+            Padding(padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: SlideTransition(
+              position: slideAnimation,
 
-                        SizedBox(height: 25,),
-                        Container(
-                          height: 50,
-                          width: 100,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.blueGrey,
-
-                          ),
-                          child: TextButton(
-                              onPressed: () {},
-                              child: Text('click'),
-                          ),
-                        )
-                      ],
+              child: Column(
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Username',
                     ),
                   ),
-                ),
+
+                  const SizedBox(height: 16.0,),
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Password'
+                    ),
+                  ),
+
+                  SizedBox(height: 20.0,),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Submit'))
+                ],
+              ),
+            ),
             )
           ],
         ),
