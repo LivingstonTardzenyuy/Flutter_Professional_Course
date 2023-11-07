870ae6ff5b57e5fe031442421e5340b487e002a1
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-07 17:36:58 +0100

    almost done with the UI of tasks_screens.dart

--- continuation of Flutter course/last30days/lib/screens/tasks_screens.dart
+++ continuation of Flutter course/last30days/lib/screens/tasks_screens.dart
@@ -5,6 +5,70 @@ class TaskScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        shape: CircleBorder(),
+        onPressed: () {},
+        child: Icon(Icons.add,),
+      ),
+
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.list, color: Colors.lightBlueAccent, size: 30.0,)),
+
+                const SizedBox(height: 20,),
+                Text('Todo', style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.w700,
+                ),),
+
+                Row(
+                  children: [
+                    Text('12', style: TextStyle(color: Colors.white, fontSize: 20),),
+                    const SizedBox(width: 12,),
+                    Text('Tasks', style: TextStyle(color: Colors.white, fontSize: 20),)
+                  ],
+                ),
+
+
+              ],
+            ),
+          ),
+
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      topLeft: Radius.circular(20))
+
+              ),
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('Thi'),
+                  )
+                ],
+              )
+            ),
+          ),
+        ],
+      ),
+
+
+    );
   }
 }
