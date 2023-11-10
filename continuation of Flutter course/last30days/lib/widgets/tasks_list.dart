67d65677e89ded36bd3de826cebdcb8812aba1c6
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-10 06:52:14 +0100

    started 8

--- continuation of Flutter course/last30days/lib/widgets/tasks_list.dart
+++ continuation of Flutter course/last30days/lib/widgets/tasks_list.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:todoss/widgets/tasktile.dart';
 
-class TaskList extends StatelessWidget {
+import '../models/task.dart';
+
+class TaskList extends StatefulWidget {
   const TaskList({super.key});
 
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+
+  List<Task> taks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy Bread'),
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-         TaskTile(),
-         TaskTile(),
-         TaskTile(),
+        
         TaskTile(),
         TaskTile(),
         TaskTile(),
--- continuation of Flutter course/last30days/lib/widgets/tasktile.dart
+++ continuation of Flutter course/last30days/lib/widgets/tasktile.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:todoss/widgets/task_check_box.dart';
 
-class TaskTile extends StatefulWidget {
-  @override
-  State<TaskTile> createState() => _TaskTileState();
-}
+class TaskTile extends StatelessWidget {
+ final bool isChecked;
+ final String title;
+ TaskTile({required this.title,required this.isChecked});
 
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = false;
 
-  void toggleCheckedState(bool? checkboxState) {
-    setState(() {
-      isChecked = checkboxState ?? false;
-    });
-  }
+  // void toggleCheckedState(bool? checkboxState) {
+  //   setState(() {
+  //     isChecked = checkboxState ?? false;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -22,14 +20,18 @@ class _TaskTileState extends State<TaskTile> {
         'This is a task',
         style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
-      trailing: TaksCheckBox(
-        isChecked: isChecked,
-        checkboxCallback: (bool? checkboxState) {
-          setState(() {
-            isChecked = checkboxState ?? false;
-          });
-        },
-      ),
+      trailing: Checkbox(
+        // activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        // onChanged: checkboxCallback,
+      );
     );
   }
 }
+
+//
+// (bool? checkboxState) {
+// setState(() {
+// isChecked = checkboxState ?? false;
+// });
+// },
