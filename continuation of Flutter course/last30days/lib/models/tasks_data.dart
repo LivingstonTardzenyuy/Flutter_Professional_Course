4b90a072abd79e665ea0ea4e54829e39448966b6
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-12 06:39:23 +0100

    provier

--- continuation of Flutter course/last30days/lib/models/tasks_data.dart
+++ continuation of Flutter course/last30days/lib/models/tasks_data.dart
@@ -2,15 +2,15 @@ import 'package:flutter/foundation.dart';
 import 'task.dart';
 
 class TaskData extends ChangeNotifier{
-  List<Task> tasks = [];
+  List<Task> _tasks = [];
 
   int get taskCount{
-    return tasks.length;
+    return _tasks.length;
   }
 
   void addTask(String newTaskTitle){
     final task = Task(name: newTaskTitle);
-    tasks.add(task);
+    _tasks.add(task);
 
     notifyListeners();
   }
