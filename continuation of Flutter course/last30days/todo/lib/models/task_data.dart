a204e8266cf7858fa7130320bd95c6caf55544a8
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-14 07:44:57 +0100

    added more security to add my task

--- continuation of Flutter course/last30days/todo/lib/models/task_data.dart
+++ continuation of Flutter course/last30days/todo/lib/models/task_data.dart
@@ -1,22 +1,27 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'tasks.dart';
 
 class TaskData extends ChangeNotifier{
-  List<Task> task = [
+  List<Task> _task = [
     Task(name: 'Buy Milk'),
     Task(name: 'Buy eggs'),
     Task(name: 'Buy Bread'),
     Task(name: 'Buy Coffee'),
   ];
 
+  UnmodifiableListView<Task> get task{
+    return UnmodifiableListView(_task);
+  }
   int get taskCount {
-    return task.length;
+    return _task.length;
   }
 
 
   void AddATask(String newTaskTitle){
   final tas = Task(name: newTaskTitle);
-  task.add(tas);
+  _task.add(tas);
   notifyListeners();
   }
 }
