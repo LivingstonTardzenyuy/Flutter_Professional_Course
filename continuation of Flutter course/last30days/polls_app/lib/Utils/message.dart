992801f7e15564804bba0506d84c7ad5500f8cd3
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-19 12:30:35 +0100

    working on firebase

---
+++ continuation of Flutter course/last30days/polls_app/lib/Utils/message.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../styles/colors.dart';
+
+void error(BuildContext? context, {required String message}){
+  ScaffoldMessenger.of(context!).showSnackBar(SnackBar(content: Text(message), backgroundColor: AppColors.red,));
+}
+
+
+void success(BuildContext? context, {required String message}){
+ScaffoldMessenger.of(context!).showSnackBar(SnackBar(content: Text(message), backgroundColor: AppColors.primaryColor,));
+}
+
+
