84b0133a9ec0ea6c63e30ffe06d4d2652db1febe
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-21 17:40:40 +0100

    working to make sure a user is authentificated

--- continuation of Flutter course/last30days/polls/lib/Screens/splash_screens.dart
+++ continuation of Flutter course/last30days/polls/lib/Screens/splash_screens.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:polls/Utils/router.dart';
+
+import 'authentification/auth_page.dart';
 
 class SplashScreen extends StatefulWidget {
 
@@ -7,13 +11,27 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  User? user = FirebaseAuth.instance.currentUser;
+
+  void navigate(){
+    Future.delayed(const Duration(seconds: 3), () {
+      if(user == null) {
+        nextPageOnly(context, AuthPage());
+      }
+        else{
+          // nextPageOnly(context, Main)
+      }
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
           child: Center(
             child: Container(
-              child: Text('i am learning flutter. So i am programming ')
+              child: FlutterLogo(),
             ),
           ),
         )
