bd17a05958c4928290766d1f80141786326505a6
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-11-23 01:42:55 +0100

    working on adding a new poll

--- continuation of Flutter course/last30days/pollsv2/lib/Screens/BottomNavPages/myPolls/add_new_polls.dart
+++ continuation of Flutter course/last30days/pollsv2/lib/Screens/BottomNavPages/myPolls/add_new_polls.dart
@@ -12,12 +12,38 @@ class _AddPollPageState extends State<AddPollPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: Text('Add a new Pole'),
       ),
 
-      body: Center(
-        child: Text('add pole'),
-      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Form(
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width - 100,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(child: Text('Post Poll', style: TextStyle(color: Colors.white),)),
+                      ),
+                    )
+                  ],
+                ),
+
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
