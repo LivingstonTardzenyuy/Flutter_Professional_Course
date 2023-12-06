cb24ea691ccee0716696fb56f1790838e2ef3bfa
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-12-06 03:02:35 +0100

    trying to add spinner.

--- continuation of Flutter course/last30days/flash_chats/lib/Screens/registration_screen.dart
+++ continuation of Flutter course/last30days/flash_chats/lib/Screens/registration_screen.dart
@@ -8,6 +8,7 @@ import 'package:provider/provider.dart';
 import '../Provider/auth_provider.dart';
 import '../components/rounded_button.dart';
 import '../constant.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   @override
@@ -19,92 +20,96 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   late String _email;
   late String _password;
   late String _confirmPassword;
+  bool _showSpinner = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Consumer<AuthProviderD>(
-        builder: (BuildContext context, AuthProviderD signup, Widget? child) {
-          return SingleChildScrollView(
-            child: Center(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(top: 50.0),
-                      child: Hero(
-                        tag: "logo",
-                        child: Container(
-                          child: Image.asset('images/logo.png'),
-                          height: 200.0,
+      body: ModalProgressHUD(
+        inAsyncCall: _showSpinner,
+        child: Consumer<AuthProviderD>(
+          builder: (BuildContext context, AuthProviderD signup, Widget? child) {
+            return SingleChildScrollView(
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50.0),
+                        child: Hero(
+                          tag: "logo",
+                          child: Container(
+                            child: Image.asset('images/logo.png'),
+                            height: 200.0,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 48.0,
-                    ),
-                    TextField(
-                      textAlign: TextAlign.center,
-                      keyboardType: TextInputType.emailAddress,
-                      onChanged: (value) {
-                        _email = value;
-                        print(_email);
-                      },
-
-                      decoration: kInputDecoration.copyWith(hintText: "Enter your Email"),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    TextField(
-                        obscureText: true,
+                      SizedBox(
+                        height: 48.0,
+                      ),
+                      TextField(
                         textAlign: TextAlign.center,
+                        keyboardType: TextInputType.emailAddress,
                         onChanged: (value) {
-                          _password = value;
-                          print(_password);
+                          _email = value;
+                          print(_email);
                         },
-                        decoration: kInputDecoration.copyWith(hintText: "Enter your password ")
 
-                    ),
+                        decoration: kInputDecoration.copyWith(hintText: "Enter your Email"),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      TextField(
+                          obscureText: true,
+                          textAlign: TextAlign.center,
+                          onChanged: (value) {
+                            _password = value;
+                            print(_password);
+                          },
+                          decoration: kInputDecoration.copyWith(hintText: "Enter your password ")
 
-                    SizedBox(height: 20,),
-                    TextField(
-                        obscureText: true,
-                        textAlign: TextAlign.center,
-                        onChanged: (value) {
-                          _confirmPassword = value;
-                          print(_confirmPassword);
-                        },
-                        decoration: kInputDecoration.copyWith(hintText: "Enter your password ")
+                      ),
 
-                    ),
-                    SizedBox(
-                      height: 24.0,
-                    ),
+                      SizedBox(height: 20,),
+                      TextField(
+                          obscureText: true,
+                          textAlign: TextAlign.center,
+                          onChanged: (value) {
+                            _confirmPassword = value;
+                            print(_confirmPassword);
+                          },
+                          decoration: kInputDecoration.copyWith(hintText: "Enter your password ")
 
-                    RoundedButton(color: Colors.blueAccent, label: "Register",
-                        onTap: () async {
-                          if(_email == "" || _password == ""){
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("All fields are required"), backgroundColor: Colors.red,));
-                          } else if (_password != _confirmPassword){
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Both passwords must be the same"), backgroundColor: Colors.red,));
-                          } else {
-                              User? result = await signup.register(email: _email, password: _password);
-                              if (result != null){
-                                Navigator.pushNamed(context, ChatRoutes.chat_screen);
+                      ),
+                      SizedBox(
+                        height: 24.0,
+                      ),
+
+                      RoundedButton(color: Colors.blueAccent, label: "Register",
+                          onTap: () async {
+                            if(_email == "" || _password == ""){
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("All fields are required"), backgroundColor: Colors.red,));
+                            } else if (_password != _confirmPassword){
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Both passwords must be the same"), backgroundColor: Colors.red,));
+                            } else {
+                                User? result = await signup.register(email: _email, password: _password);
+                                if (result != null){
+                                  Navigator.pushNamed(context, ChatRoutes.chat_screen);
+                              }
                             }
-                          }
-                        })
-                  ],
+                          })
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
 
+        ),
       ),
     );
   }
