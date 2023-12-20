2e703a94338b66fb492a23d2cb178da27b86e979
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2023-12-20 04:42:58 +0100

    connected my app to firebase

--- continuation of Flutter course/last30days/ref_app/lib/main.dart
+++ continuation of Flutter course/last30days/ref_app/lib/main.dart
@@ -1,4 +1,5 @@
 // import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ref_app/provider/auth_profile.dart';
@@ -20,7 +21,9 @@ import 'package:ref_app/screen/splash.dart';
 //   }
 // }
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ReferalApp());
 }
 
